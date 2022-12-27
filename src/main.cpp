0ac694f185945790f795fff7593884aeda695aef
Matthew Evans <mevans+git@wtfsystems.net>
2022-12-27 11:01:24 -0500

    sync

--- src/main.cpp
+++ src/main.cpp
@@ -25,6 +25,147 @@ int main(int argc, char **argv) {
     wte::config::controls::p1_key_action1 = ALLEGRO_KEY_RCTRL;
     wte::config::controls::p1_key_action2 = ALLEGRO_KEY_ALTGR;
 
+    /* ********************************* */
+    /* *** Input handling ************** */
+    /* ********************************* */
+    wte::add_handler<wte::NONGAME_HANDLES, wte::EVENT_KEY_DOWN, wte::handler::key>([](const int& key, ALLEGRO_DISPLAY* display) {
+        //wte::mgr::messages::add(wte::message("system", "new-game", "game.sdf"));
+    });
+
+    wte::add_handler<wte::GAME_HANDLES, wte::EVENT_KEY_DOWN, wte::handler::key>([](const int& key, ALLEGRO_DISPLAY* display) {
+        if(key == wte::config::controls::p1_key_up) {
+            wte::entity_id player_id = wte::mgr::world::get_id("player");
+            player_pols::y = -1.0f;
+            const float rad = std::atan2(player_pols::y, player_pols::x);
+            wte::mgr::world::set_component<wte::cmp::motion>(player_id)->direction = rad;
+            wte::mgr::world::set_component<wte::cmp::motion>(player_id)->x_vel = 5.0f;
+            wte::mgr::world::set_component<wte::cmp::motion>(player_id)->y_vel = 5.0f;
+        }
+        if(key == wte::config::controls::p1_key_down) {
+            wte::entity_id player_id = wte::mgr::world::get_id("player");
+            player_pols::y = 1.0f;
+            const float rad = std::atan2(player_pols::y, player_pols::x);
+            wte::mgr::world::set_component<wte::cmp::motion>(player_id)->direction = rad;
+            wte::mgr::world::set_component<wte::cmp::motion>(player_id)->x_vel = 5.0f;
+            wte::mgr::world::set_component<wte::cmp::motion>(player_id)->y_vel = 5.0f;
+        }
+        if(key == wte::config::controls::p1_key_left) {
+            wte::entity_id player_id = wte::mgr::world::get_id("player");
+            player_pols::x = -1.0f;
+            const float rad = std::atan2(player_pols::y, player_pols::x);
+            wte::mgr::world::set_component<wte::cmp::motion>(player_id)->direction = rad;
+            wte::mgr::world::set_component<wte::cmp::motion>(player_id)->x_vel = 5.0f;
+            wte::mgr::world::set_component<wte::cmp::motion>(player_id)->y_vel = 5.0f;
+        }
+        if(key == wte::config::controls::p1_key_right) {
+            wte::entity_id player_id = wte::mgr::world::get_id("player");
+            player_pols::x = 1.0f;
+            const float rad = std::atan2(player_pols::y, player_pols::x);
+            wte::mgr::world::set_component<wte::cmp::motion>(player_id)->direction = rad;
+            wte::mgr::world::set_component<wte::cmp::motion>(player_id)->x_vel = 5.0f;
+            wte::mgr::world::set_component<wte::cmp::motion>(player_id)->y_vel = 5.0f;
+        }
+        if(key == wte::config::controls::p1_key_action1) {
+            wte::entity_id player_id = wte::mgr::world::get_id("player");
+            wte::entity_id can_id = wte::mgr::world::get_id("main_cannon");
+            //  Set the cannon's location to match the player.
+            wte::mgr::world::set_component<wte::cmp::location>(can_id)->pos_x = 
+                wte::mgr::world::get_component<wte::cmp::location>(player_id)->pos_x;
+            wte::mgr::world::set_component<wte::cmp::location>(can_id)->pos_y = 
+                wte::mgr::world::get_component<wte::cmp::location>(player_id)->pos_y -
+                wte::mgr::world::get_component<wte::cmp::hitbox>(can_id)->height;
+
+            //  Turn the cannon on.
+            wte::mgr::world::set_component<wte::cmp::gfx::sprite>(can_id)->visible = true;
+            wte::mgr::world::set_component<wte::cmp::ai>(can_id)->enabled = true;
+            wte::mgr::world::set_component<wte::cmp::hitbox>(can_id)->solid = true;
+            //  Play sound effect.
+            wte::mgr::audio::sample::play(wte::mgr::assets::get<ALLEGRO_SAMPLE>("laser"), "cannon_fire");
+        }
+        if(key == wte::config::controls::p1_key_action2) {
+            wte::entity_id player_id = wte::mgr::world::get_id("player");
+            wte::entity_id shd_id = wte::mgr::world::get_id("shield");
+            //  Set the shield's location to match the player
+            wte::mgr::world::set_component<wte::cmp::location>(shd_id)->pos_x =
+                wte::mgr::world::get_component<wte::cmp::location>(player_id)->pos_x - 28.0f;
+            wte::mgr::world::set_component<wte::cmp::location>(shd_id)->pos_y =
+                wte::mgr::world::get_component<wte::cmp::location>(player_id)->pos_y - 16.0f;
+
+            if(wte::mgr::world::set_component<energy>(shd_id)->amt > 0) {
+                //  Enable the shield.
+                wte::mgr::world::set_component<wte::cmp::gfx::sprite>(shd_id)->visible = true;
+                wte::mgr::world::set_component<wte::cmp::ai>(shd_id)->enabled = true;
+                wte::mgr::world::set_component<wte::cmp::hitbox>(shd_id)->solid = true;
+                wte::mgr::world::set_component<wte::cmp::hitbox>(player_id)->solid = false;
+                //  Play sound effect.
+                wte::mgr::audio::sample::play(wte::mgr::assets::get<ALLEGRO_SAMPLE>("shield"), "shield_sound");
+            }
+        }
+
+        if(key == wte::config::controls::key_menu) {
+            if(wte::config::flags::engine_paused) {
+                wte::config::flags::engine_paused = false;
+            } else {
+                wte::config::flags::engine_paused = true;
+            }
+        }
+    });
+
+    wte::add_handler<wte::GAME_HANDLES, wte::EVENT_KEY_UP, wte::handler::key>([](const int& key, ALLEGRO_DISPLAY* display) {
+        if(key == wte::config::controls::p1_key_up) {
+            wte::entity_id player_id = wte::mgr::world::get_id("player");
+            if(player_pols::y < 0.0f) player_pols::y = 0.0f;
+            if(player_pols::x == 0.0f && player_pols::y == 0.0f) {
+                wte::mgr::world::set_component<wte::cmp::motion>(player_id)->x_vel = 0.0f;
+                wte::mgr::world::set_component<wte::cmp::motion>(player_id)->y_vel = 0.0f;
+            }
+        }
+        if(key == wte::config::controls::p1_key_down) {
+            wte::entity_id player_id = wte::mgr::world::get_id("player");
+            if(player_pols::y > 0.0f) player_pols::y = 0.0f;
+            if(player_pols::x == 0.0f && player_pols::y == 0.0f) {
+                wte::mgr::world::set_component<wte::cmp::motion>(player_id)->x_vel = 0.0f;
+                wte::mgr::world::set_component<wte::cmp::motion>(player_id)->y_vel = 0.0f;
+            }
+        }
+        if(key == wte::config::controls::p1_key_left) {
+            wte::entity_id player_id = wte::mgr::world::get_id("player");
+            if(player_pols::x < 0.0f) player_pols::x = 0.0f;
+            if(player_pols::x == 0.0f && player_pols::y == 0.0f) {
+                wte::mgr::world::set_component<wte::cmp::motion>(player_id)->x_vel = 0.0f;
+                wte::mgr::world::set_component<wte::cmp::motion>(player_id)->y_vel = 0.0f;
+            }
+        }
+        if(key == wte::config::controls::p1_key_right) {
+            wte::entity_id player_id = wte::mgr::world::get_id("player");
+            if(player_pols::x > 0.0f) player_pols::x = 0.0f;
+            if(player_pols::x == 0.0f && player_pols::y == 0.0f) {
+                wte::mgr::world::set_component<wte::cmp::motion>(player_id)->x_vel = 0.0f;
+                wte::mgr::world::set_component<wte::cmp::motion>(player_id)->y_vel = 0.0f;
+            }
+        }
+        if(key == wte::config::controls::p1_key_action1) {
+            //  Turn the cannon off.
+            wte::entity_id can_id = wte::mgr::world::get_id("main_cannon");
+            wte::mgr::world::set_component<wte::cmp::gfx::sprite>(can_id)->visible = false;
+            wte::mgr::world::set_component<wte::cmp::ai>(can_id)->enabled = false;
+            wte::mgr::world::set_component<wte::cmp::hitbox>(can_id)->solid = false;
+            //  Stop sound effect.
+            wte::mgr::audio::sample::stop("cannon_fire");
+        }
+        if(key == wte::config::controls::p1_key_action2) {
+            wte::entity_id player_id = wte::mgr::world::get_id("player");
+            wte::entity_id shd_id = wte::mgr::world::get_id("shield");
+            //  Disable shield.
+            wte::mgr::world::set_component<wte::cmp::gfx::sprite>(shd_id)->visible = false;
+            wte::mgr::world::set_component<wte::cmp::ai>(shd_id)->enabled = false;
+            wte::mgr::world::set_component<wte::cmp::hitbox>(shd_id)->solid = false;
+            wte::mgr::world::set_component<wte::cmp::hitbox>(player_id)->solid = true;
+            //  Stop sound effect.
+            wte::mgr::audio::sample::stop("shield_sound");
+        }
+    });
+
     //  Load settings.
     wte::config::load();
 
